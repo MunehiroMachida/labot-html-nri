88a48cd06667014882b82c7fa664055c5072caeb
MUnehiroMachida <life1231@outlook.jp>
2020-10-27 19:54:39 +0900

    ハンバーガーのアイコン

--- html-kaguya/js/script.js
+++ html-kaguya/js/script.js
@@ -1,5 +1,13 @@
+// ページ遷移
 const body = document.querySelector('body')
 body.animate({
     opacity: [0, 1]
 }, 1500)
 
+// ハンバーガーをクリックした時の処理
+$(function () {
+    $('.sp-hamburger').on('click', function () {
+        $(this).toggleClass('active');
+        return false;
+    });
+});
